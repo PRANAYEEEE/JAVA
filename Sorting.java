8be0f8ed81e3ee675b5a726f49b2b52971520514
pmolleti_in <pranay.molleti@oracle.com>
2024-03-31 23:25:14 +0530

    done

--- Sorting.java
+++ Sorting.java
@@ -1,5 +1,5 @@
 public class Sorting {
-    //This is a sample
+    //This is a sample code
     public static void BubbleSort(int[] nums){
         for (int i = 0; i < nums.length-1; i++) {
             for (int j = 1; j < nums.length-i; j++) {
